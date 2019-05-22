e94df5d22d52205d16d7f4711e771477d5f2967c
Olga <44804147+OlgaMazo@users.noreply.github.com>
2019-05-22 12:42:15 +0300

    add get function

--- LineCounter.java
+++ LineCounter.java
@@ -4,25 +4,37 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * This class represents the thread that calculates the line number of the file.
- * @author Daniel and Olga
+ * LineCounter
+ * This class represents a Thread that count how many rows there are in one file
+ * @author Olga and Daniel
  */
 public class LineCounter extends Thread {
-
-	String fileName;
-	int rows = 0;
-
+	
+	private String file_name;
+	private int rows = 0;
+	
 	/**
-	 * LineCounter constractor
-	 * @param fileName - the file name
+	 * LineCounter
+	 * constructor that receive file's name
+	 * @param file_name - file's name 
 	 */
-	public LineCounter(String fileName) {
-		this.fileName = fileName;
+	public LineCounter(String file_name) {
+		
+		this.file_name = file_name;
 	}
-
+	
+	/**
+	 * getRows
+	 * @return - how many rows
+	 */
+	public int getRows() {
+		return rows;
+	}
+	
 	/**
 	 * run
-	 * This function will be enabled at the beginning of the thread (start command).
+	 * This function will be enabled at the beginning of the thread (start command)
+	 * It calls to function that calculate how many rows in the file
 	 */
 	public void run() {
 		try {
@@ -30,46 +42,43 @@ public class LineCounter extends Thread {
 		} catch (IOException e) {
 		}
 	}
-
+	
 	/**
-	 * num_of_rows Calculates the number of rows in the file.
-	 * source: https://stackoverflow.com/questions/453018/number-of-lines-in-a-file-in-java
+	 * num_of_rows
+	 * This function will be activated when the Thread is initiated, it calculate how many rows there are in
+	 * this file
+	 * source - https://stackoverflow.com/questions/453018/number-of-lines-in-a-file-in-java
 	 * @throws IOException - I/O exception
 	 */
 	private void num_of_rows() throws IOException {
-
-		//create input stream for the file in order to read the data from it
-		InputStream is = new BufferedInputStream(new FileInputStream(fileName));
-
-		try {
-			byte[] c = new byte[1024]; //byte array in order to read the chars in the lines
-			int count = 0;
-			int readChars = 0;
-			boolean empty = true; //if the file is empty
-
-			//while the file has more lines
-			while ((readChars = is.read(c)) != -1) {
-				empty = false;
-
-				//read this line until we reach a line drop
-				for (int i = 0; i < readChars; ++i) {
-					if (c[i] == '\n') {
-						++count;
-					}
-				}
-			}
-
-			//if there is one line in the file
-			if (count == 0 && !empty) {
-				rows = 1;
-			} else {
-				rows = count;
-			}
-
-			//close the input stream
-		} finally {
-			is.close();
-		}
-
+		
+		//create an object that represent a stream in order to read the data from the file
+		InputStream is = new BufferedInputStream(new FileInputStream(file_name));
+	    try {
+	    	//byte array in order to read the chars from lines
+	        byte[] c = new byte[1024];
+	        int count = 0;
+	        int readChars = 0;
+	        //flag for the first line
+	        boolean empty = true;
+	        //while the file has more lines
+	        while ((readChars = is.read(c)) != -1) {
+	            empty = false;
+	            //go over all the line
+	            for (int i = 0; i < readChars; ++i) {
+	            	//if we got the end of the line
+	                if (c[i] == '\n') {
+	                    ++count;
+	                }
+	            }
+	        }
+	        if(count == 0 && !empty)
+	        	rows = 1;
+	        else
+	        	rows = count;
+	    //close the file    
+	    } finally {
+	        is.close();
+	    }
 	}
 }
