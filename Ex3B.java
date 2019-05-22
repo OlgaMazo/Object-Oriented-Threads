ac72d070c090ee66ccbba88212b0ca699a9cccd1
danielzilpa <44943578+danielzilpa@users.noreply.github.com>
2019-05-23 00:01:39 +0300

    Update Ex3B.java

--- Ex3B.java
+++ Ex3B.java
@@ -10,24 +10,15 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
- * Ex3B
- * In this class we can do some actions on files: create files, delete files, 
+ * In this class we can do some actions on files: create files, delete files,
  * count the number of rows in each file - this is done by threads
  * @author Olga and Daniel
  */
 public class Ex3B {
-	
-	public static void main(String[] args) {
 
-		countLinesThreads(6);
-		countLinesThreadPool(6);
-		countLinesOneProcess(6);
-
-	}
-	
 	/**
-	 *createFiles
-	 *This function creates files. The number of files will be according to the number it received.
+	 * createFiles 
+	 * This function creates files, the number of files will be according to the number it received.
 	 * @param n - how many files
 	 * @return - array of String's with the file's names
 	 */
@@ -36,23 +27,20 @@ public class Ex3B {
 		String[] filesNames = new String[n];
 		Random r = new Random(123);
 		int numLines = 0;
-		//the files name will be File_i
-		String fileName = "File_";
+		String fileName = "File_"; //the files name will be File_i
 
-		//creating n files
+		//create n files
 		for (int i = 1; i <= n; i++) {
 
 			//dragging the number of rows in a file
 			numLines = r.nextInt(1000);
-			fileName = fileName + i;			
+			fileName = fileName + i;
 			File file = new File(fileName);
-			filesNames[i-1] = fileName;
+			filesNames[i - 1] = fileName;
 
 			try {
-
 				if (!file.exists()) {
-					//create the file
-					file.createNewFile();
+					file.createNewFile(); //create the file
 				}
 
 				//with this variable we can write in the file
@@ -62,199 +50,221 @@ public class Ex3B {
 				for (int j = 0; j < numLines; j++) {
 					pw.println("Hello World");
 				}
-				
+
 				//close the print writer
 				pw.close();
 
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
+
 			fileName = "File_";
 		}
 		//return array with the file's name
 		return filesNames;
 	}
-	
+
 	/**
-	 *deleteFiles
-	 *This function receives an array with file names and deletes them
-	 *@param fileNames - the file's names that will be deleted
+	 * deleteFiles 
+	 * This function receives an array with file names and deletes them.
+	 * @param fileNames - the file's names that will be deleted
 	 */
 	public static void deleteFiles(String[] fileNames) {
-		
+
 		//create routing of the folder in which the files are stored
 		final String dir = System.getProperty("user.dir");
-		
+
 		//go through all the files and delete them
-		for(int i=0; i<fileNames.length; i++) {
+		for (int i = 0; i < fileNames.length; i++) {
 			File file = new File(dir + "\\" + fileNames[i]);
 			file.delete();
 		}
 	}
-	
+
 	/**
-	 *countLinesThreads
-	 *The function receives several files to be created. The function creates files, triggers for each file the
-	 *thread and prints the total number of rows in all files. It also prints the runtime of the threads and eventually deletes all the files
-	 *@param numFiles - the number of files we need to create
+	 * countLinesThreads 
+	 * The function receives multiple files as an argument. The
+	 * function creates files, triggers for each file the thread and prints the
+	 * total number of rows in all files. It also prints the runtime of the threads
+	 * and eventually deletes all the files
+	 * @param numFiles - the number of files we need to create
 	 */
 	public static void countLinesThreads(int numFiles) {
-		
-		//create numFiles
-		String[] filesNames = createFiles(numFiles);
+
+		String[] filesNames = createFiles(numFiles); //create numFiles
 		int totalRows = 0;
 		long totalTime = 0;
-		
+
 		//go through all the files
-		for(int i=0; i<filesNames.length; i++) {
-				
+		for (int i = 0; i < filesNames.length; i++) {
+
 			//the start time of the thread
 			long startTime = System.nanoTime();
+
 			//create LineCounter to calculate how many rows in the file
 			LineCounter lc = new LineCounter(filesNames[i]);
+
 			//start the thread
 			lc.start();
+
 			//the end time of the thread
 			long endTime = System.nanoTime();
-			
-			//total time
-			totalTime = totalTime + (endTime-startTime);
-			
+
 			try {
 				lc.join();
-			} catch (InterruptedException e) {}
-			
-			//sum all the rows
-			totalRows += lc.getRows();
+			} catch (InterruptedException e) {
 			}
-		
-		System.out.println("Total running time (countLinesThreads): " + totalTime + " nano seconds");
+
+			//sum all the rows
+			totalRows += lc.get_rows();
+
+			//total running time of the threads
+			totalTime = totalTime + (endTime - startTime);
+		}
+
+		System.out.println("Total running time (countLinesThreads): " + totalTime + " nano-seconds");
 		System.out.println("Total rows in all the files: " + totalRows);
-		
+
 		//delete all the files
 		deleteFiles(filesNames);
 	}
-	
+
 	/**
-	 *countLinesThreadPool
-	 *The function receives several files to be created, triggering for each file the thread created in TreadPool
-	 *and prints the total number of rows in all files. It also prints the runtime of the threads and eventually deletes all the files
-	 *@param num - the number of files we need to create
+	 * countLinesThreadPool
+	 * The function creates files, triggers for each file the thread that C=created in TreadPool 
+	 * and prints the run time and total number of rows in all files.
+	 * @param num - the files number
 	 */
 	public static void countLinesThreadPool(int num) {
-		
-		//create num files
+
+		//create numFiles
 		String[] filesNames = createFiles(num);
 		int totalRows = 0;
 		long totalTime = 0;
-		//get ExecutorService from Executors utility class, thread pool
-		ExecutorService executor = Executors.newFixedThreadPool(num);
-		
-		//go through all the files
-		for(int i=0; i<filesNames.length; i++) {
 
-			//start time
+		//with this object we have static services to create Thread repositories
+		ExecutorService executor = Executors.newFixedThreadPool(num);
+
+		//go through all the files
+		for (int i = 0; i < filesNames.length; i++) {
+
+			//the start time of the thread
 			long startTime = System.nanoTime();
+
+			//create LineCounter2 object to calculate how many rows in the file
 			LineCounter2 lc = new LineCounter2(filesNames[i]);
-			//the number of rows will be in future
+
+			//perform "call" function, the answer is to be saved in the future object
 			Future<Integer> future = executor.submit(lc);
-			//end time
-			long endTime = System.nanoTime();	
-			//total time
-			totalTime = totalTime + (endTime-startTime);
-						
+
+			//the end time of the thread
+			long endTime = System.nanoTime();
+
+			//sum all the rows
 			try {
-				totalRows += future.get();
+				totalRows = totalRows + future.get();
 			} catch (Exception e) {
-				
 				e.printStackTrace();
-			} 
+			}
+
+			//total running time of the threads
+			totalTime = totalTime + (endTime - startTime);
 		}
-		
-		System.out.println("Total running time (countLinesThreadPool): " + totalTime + " nano seconds");
+
+		System.out.println("Total running time (countLinesThreadPool): " + totalTime + " nano-seconds");
 		System.out.println("Total rows in all the files: " + totalRows);
 
-		
-		//delete all files
+		//delete all the files
 		deleteFiles(filesNames);
-		//stop the thread
+
+		//shutdown the executor service object
 		executor.shutdown();
-		}
-	
-	
+	}
+
 	/**
-	 *countLinesOneProcess
-	 *The function receives a number of files to create, creates, calculates and prints a few lines in total
-	 *It also prints the runtime of the threads and eventually deletes all the files
-	 *@param numFiles - the number of files we need to create
+	 * countLinesOneProcess
+	 * The function creates files, reads files one by one, prints the total number of rows and the run time without using threads.
+	 * @param numFiles -  - the files number
 	 */
 	public static void countLinesOneProcess(int numFiles) {
-		
+
 		//create numFiles
 		String[] filesNames = createFiles(numFiles);
 		int totalRows = 0;
 		long totalTime = 0;
 		int rows = 0;
-		
+
 		//go through all the files
-		for(int i=0; i<filesNames.length; i++) {
-			
-			//start time
+		for (int i = 0; i < filesNames.length; i++) {
+
+			//the start time of the thread
 			long startTime = System.nanoTime();
+
+			//calculate the file rows by "count_rows" function
 			try {
-				//check how many rows there are in the file
-				rows = numOfRows(filesNames[i]);
+				rows = count_rows(filesNames[i]);
 			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			
-			//end time
+
+			//the end time of the thread
 			long endTime = System.nanoTime();
-			totalTime = totalTime + (endTime-startTime);
-			totalRows += rows;	
+
+			//sum all the rows
+			totalRows = totalRows + rows;
+
+			//total running time of the threads
+			totalTime = totalTime + (endTime - startTime);
 		}
-		
-		System.out.println("Total running time of the threads (countLinesOneProcess): " + totalTime + " nano seconds");
+
+		System.out
+				.println("Total running time of the threads (countLinesOneProcess): " + totalTime + " nano-seconds");
 		System.out.println("Total rows in all the files: " + totalRows);
-		
-		//delete all files
+
+		//delete all the files
 		deleteFiles(filesNames);
 	}
-	
+
 	/**
-	 * numOfRows
-	 * This function calculate how many rows there are in a file
-	 * source - https://stackoverflow.com/questions/453018/number-of-lines-in-a-file-in-java
-	 * @param filesNames - the file that we check
-	 * @throws IOException - I/O exception
+	 * count_rows
+	 * count the file rows
+	 * @param fileName - the file name
+	 * @return the file rows
+	 * @throws IOException - input/output exception
 	 */
-	private static int numOfRows(String filesNames) throws IOException {
-		//create an object that represent a stream in order to read the data from the file
-	    InputStream is = new BufferedInputStream(new FileInputStream(filesNames));
-	    try {
-	    	//byte array in order to read the chars from lines
-	        byte[] c = new byte[1024];
-	        int count = 0;
-	        int readChars = 0;
-	        //flag for the first line
-	        boolean empty = true;
-	        
-	        //while the file has more lines
-	        while ((readChars = is.read(c)) != -1) {
-	            empty = false;
-	            //go over all the line
-	            for (int i = 0; i < readChars; ++i) {
-	            	//if we got the end of the line
-	                if (c[i] == '\n') {
-	                    ++count;
-	                }
-	            }
-	        }     
-	        return (count == 0 && !empty) ? 1 : count;
-	        
-	    } finally {
-	    	//close the file
-	        is.close();
-	    }
+	private static int count_rows(String fileName) throws IOException {
+
+		//create input stream for the file in order to read the data from it
+		InputStream is = new BufferedInputStream(new FileInputStream(fileName));
+
+		try {
+			byte[] c = new byte[1024]; //byte array in order to read the chars in the lines
+			int count = 0;
+			int readChars = 0;
+			boolean empty = true; //if the file is empty
+
+			//while the file has more lines
+			while ((readChars = is.read(c)) != -1) {
+				empty = false;
+
+				//read this line until we reach a line drop
+				for (int i = 0; i < readChars; ++i) {
+					if (c[i] == '\n') {
+						++count;
+					}
+				}
+			}
+
+			//if there is one line in the file
+			if (count == 0 && !empty) {
+				return 1;
+			} else {
+				return count;
+			}
+
+			//close the input stream
+		} finally {
+			is.close();
+		}
 	}
 }
